0a550615a486da77001755e397e8699408826485
Nathan Jones <najones.arch@gmail.com>
2017-06-21 14:56:58 -0500

    Gifs, Fonts, Awesomeness
    Utilize font awesome icons, add a little color -
    prepare for gif awesomeness on next push.

--- source/assets/javascripts/hoverText.js
+++ source/assets/javascripts/hoverText.js
@@ -1,17 +1,17 @@
 // $("#new-attributes-link").on("mouseover", function(e) {
 //   console.log("link is hovered");
 // });
-
-document.addEventListener("DOMContentLoaded", function(){
-  var newAttitudes = document.querySelector("#new-attitudes-link");
-
-  newAttitudes.addEventListener('mouseover', function(e){
-    document.body.classList.add('link-is-hovered');
-    console.log('link is hovered');
-  });
-  newAttitudes.addEventListener('mouseout', function(e){
-    document.body.classList.remove('link-is-hovered');
-    console.log('link is not hovered');
-  });
-  // Handler when the DOM is fully loaded
-});
+// 
+// document.addEventListener("DOMContentLoaded", function(){
+//   var newAttitudes = document.querySelector("#new-attitudes-link");
+//
+//   newAttitudes.addEventListener('mouseover', function(e){
+//     document.body.classList.add('link-is-hovered');
+//     console.log('link is hovered');
+//   });
+//   newAttitudes.addEventListener('mouseout', function(e){
+//     document.body.classList.remove('link-is-hovered');
+//     console.log('link is not hovered');
+//   });
+//   // Handler when the DOM is fully loaded
+// });
